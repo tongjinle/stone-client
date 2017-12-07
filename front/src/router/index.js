67775588cb9568315be647b8de9a22a63352d7d3
zhangjingjie <love58615050>
2017-12-08 00:57:09 +0800

    1.add common/util 2.add components/recommend/recommend 3.add base/loading/loading 4.add base/scroll/scroll  ...(*￣０￣)ノ

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Recommend from 'components/recommend/recommend'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/recommend'
+    },{
+      path: '/recommend',
+      name: 'recommend',
+      component: Recommend
     }
   ]
 })
